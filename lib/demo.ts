f0e1be92ee75f9cad1d92b10f117455457b07cfd
Faisal Sayyed <sayyedfaisal06@gmail.com>
2024-08-13 01:53:47 +0530

    demo

---
+++ lib/demo.ts
@@ -0,0 +1,4 @@
+// CONVEX_DEPLOYMENT=dev:strong-bulldog-652
+// NEXT_PUBLIC_CONVEX_URL=https://strong-bulldog-652.convex.cloud
+// NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY=pk_test_c2luY2VyZS1nbmF0LTQwLmNsZXJrLmFjY291bnRzLmRldiQ
+// CLERK_SECRET_KEY=sk_test_HAXOghfrTXjTmXbV99YpK38IJkILrq5U3fkSnLjbTx
