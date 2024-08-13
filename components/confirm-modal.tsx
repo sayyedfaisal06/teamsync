abaa0819241bb4def02253c2738b73ff9cf83f9b
Faisal Sayyed <sayyedfaisal06@gmail.com>
2024-08-13 23:09:57 +0530

    made change to delete board

---
+++ components/confirm-modal.tsx
@@ -0,0 +1,55 @@
+"use client";
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  AlertDialogPortal,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+type ConfirmModalProps = {
+  children: React.ReactNode;
+  onConfirmClick: () => void;
+  header: string;
+  description?: string;
+  disabled?: boolean;
+};
+
+const ConfirmModal = ({
+  children,
+  header,
+  onConfirmClick,
+  description,
+  disabled,
+}: ConfirmModalProps) => {
+  const handleConfirm = () => {
+    onConfirmClick();
+  };
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>{children}</AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{header}</AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogDescription>{description}</AlertDialogDescription>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction disabled={disabled} onClick={handleConfirm}>
+            Confirm
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default ConfirmModal;
